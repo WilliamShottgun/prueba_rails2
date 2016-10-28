8dbcee8302d189f5f0342c397f0a3a5cae115351
vdelamazah <vdelamazah@gmail.com>
2016-10-28 17:10:20 -0300

    se agrega campo premium, price

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,13 @@
 class Product < ApplicationRecord
   belongs_to :category
+  validates :price, presence: true
+	before_create :premium 
+	
+	def premium
+		unless self.premium = true
+			self.premium = false
+		end
+	end
+
+
 end
---
+++ db/migrate/20161028192527_add_price_from_product.rb
@@ -0,0 +1,4 @@
+class AddPriceFromProduct < ActiveRecord::Migration[5.0]
+  def change
+  end
+end
